e1c881f7e179c9495e7517c30d95a1e4c78bde1c
wangchao <2893324092@qq.com>
2020-05-06 18:56:56 +0800

    项目第一天

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import zhuce from '@/views/zhuce'
+import zhuceyi from '@/views/zhuceyi'
+
 
 Vue.use(Router)
 
@@ -10,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/zhuce',
+      name: 'zhuce',
+      component: zhuce
+    },
+    {
+      path: '/zhuceyi',
+      name: 'zhuceyi',
+      component: zhuceyi
     }
   ]
 })
---
+++ src/views/zhuceyi.vue
@@ -0,0 +1,115 @@
+<template>
+    <div>
+        <div class="jiafang">
+            <ul>
+                <li class="li1">
+                    <div>注册开发者账号</div>
+                </li>
+                <li class="li2">
+                    <input placeholder=" 用户名(即个性后缀，注册后无法修改)"/>
+                </li>
+                <li class="li3">
+                    <div class="li3_1">
+                        <input type="text" class="li3_1_1"/>
+                        <input type="button" class="li3_1_2"/>
+                    </div>
+                </li>
+                <li class="li4">
+                    <input placeholder=" 输入手机验证码"/>
+                </li>
+                <li class="li5">
+                    <input placeholder=" 请输入密码"/>
+                </li>
+                <li class="li6">
+                    <input placeholder=" 请确认项目"/>
+                </li>
+                <li class="li7">
+                    <div>
+                        <input type="checkbox"/>
+                        <div>我同意遵守<a href="#">《码市用户服务协议》</a></div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.jiafang{
+    width: 360px;
+    height: 500px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin: 30px auto;
+}
+.jiafang ul{
+    list-style: none;
+}
+.jiafang ul li{
+    display: block;
+    width: 320px;
+    height: 40px;
+    margin-top: 20px;
+    margin-left: 20px;
+}
+.jiafang ul li input{
+    width: 320px;
+    height: 40px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    text-indent: 5px;
+    outline: none;
+}
+.jiafang ul .li1{
+    width: 320px;
+    height: 40px;
+    margin-top: 20px;
+    margin-left: 20px;
+    text-align: center;
+    font-size: 23px;
+}
+.jiafang ul .li7{
+    width: 320px;
+    height: 40px;
+    float: left;
+    
+}
+.jiafang ul .li7 input{
+    width: 14px;
+    height: 14px;
+    float: left;
+    margin-top: 3px;
+}
+.jiafang ul .li7 div div{
+    font-size: 14px;
+}
+.jiafang ul .li3_1{
+    width: 320px;
+    height: 40px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+.jiafang ul .li3_1 input{
+    height: 40px;
+}
+.jiafang ul .li3_1 .li3_1_1{
+    width: 200px;
+    border-radius: 5px 0px 0px 5px;
+    float: left;
+    border-right: 0px solid #fff;
+}
+.jiafang ul .li3_1 .li3_1_2{
+    width: 119px;
+    float: left;
+    border-right: 0px solid #fff;
+    border-radius: 0px 5px 5px 0px;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
